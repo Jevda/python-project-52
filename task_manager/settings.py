c719d5d6b89ef8d9233110edf78152f0c34dfdcf
Jevda <alekzanya@gmail.com>
2025-04-30 17:01:55 +0300

    Шаг 7: Подстройка под тесты GitHub

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -1,5 +1,4 @@
 # task_manager/settings.py
-# Стандартный файл настроек Django для проекта task_manager.
 import os
 import dj_database_url
 from dotenv import load_dotenv
@@ -14,33 +13,31 @@ ALLOWED_HOSTS = [
     'webserver',
     '127.0.0.1',
     'localhost',
-    'python-project-52-tvt9.onrender.com' # Убедись, что здесь твой актуальный URL
+    'python-project-52-tvt9.onrender.com'
 ]
 
-# Application definition
+# --- ИЗМЕНЕНИЯ ЗДЕСЬ ---
 INSTALLED_APPS = [
-    # Стандартные приложения Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # Whitenoise
-    'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic', # Должен быть перед staticfiles
     'django.contrib.staticfiles',
-    # Сторонние приложения
     'django_bootstrap5',
     'django_filters',
-    # Наши приложения
-    'users.apps.UsersConfig',
-    'statuses.apps.StatusesConfig',
-    'tasks.apps.TasksConfig',
-    'labels.apps.LabelsConfig',
+    # Наши приложения - указываем только имена
+    'users',
+    'statuses',
+    'tasks',
+    'labels',
 ]
+# --- КОНЕЦ ИЗМЕНЕНИЙ ---
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Должен быть после SecurityMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -71,28 +68,20 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
+        # Используем SQLite локально по умолчанию
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600
     )
 }
 
-# --- ИЗМЕНЕНИЯ ЗДЕСЬ ---
-# Определяем валидаторы паролей
 AUTH_PASSWORD_VALIDATORS = [
     {
-        # Используем только валидатор минимальной длины
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        # Устанавливаем минимальную длину в 3 символа
         'OPTIONS': {
             'min_length': 3,
         }
     },
-    # Убрали остальные стандартные валидаторы:
-    # {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
-    # {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
-    # {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
 ]
-# --- КОНЕЦ ИЗМЕНЕНИЙ ---
 
 LANGUAGE_CODE = 'ru-RU'
 TIME_ZONE = 'Europe/Riga'

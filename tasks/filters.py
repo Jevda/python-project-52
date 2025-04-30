af7fab2d4155da10a59e000b9bb20f3057a9b7c7
Jevda <alekzanya@gmail.com>
2025-04-30 19:09:30 +0300

    Шаг 7: Подстройка под тесты GitHub (tasks/filters.py)

--- tasks/filters.py
+++ tasks/filters.py
@@ -2,19 +2,27 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
-# Импортируем нужные классы из django-filter
-from django_filters import FilterSet, ModelChoiceFilter, BooleanFilter
+# Импортируем ChoiceFilter вместо ModelChoiceFilter для executor
+from django_filters import FilterSet, ModelChoiceFilter, BooleanFilter, ChoiceFilter
 
 from .models import Task
 from statuses.models import Status
 from labels.models import Label
 
-# Убираем кастомный класс UserChoiceField отсюда
+# Убираем импорт UserChoiceField, он здесь не нужен
+# from .forms import UserChoiceField
 
 # Класс Фильтра
 class TaskFilter(FilterSet):
-    # Используем СТАНДАРТНЫЙ ModelChoiceFilter для всех полей
 
+    # --- ИСПОЛЬЗУЕМ ChoiceFilter для executor ---
+    executor = ChoiceFilter(
+        label=_('Исполнитель'),
+        # Choices будут заданы в __init__ ниже
+        widget=forms.Select(attrs={'class': 'form-select'}),
+    )
+
+    # Фильтр по статусу (остается ModelChoiceFilter)
     status = ModelChoiceFilter(
         queryset=Status.objects.all(),
         label=_('Статус'),
@@ -22,13 +30,7 @@ class TaskFilter(FilterSet):
         widget=forms.Select(attrs={'class': 'form-select'}),
     )
 
-    executor = ModelChoiceFilter( # <-- Стандартный ModelChoiceFilter
-        queryset=User.objects.all(),
-        label=_('Исполнитель'),
-        empty_label=_('Все исполнители'),
-        widget=forms.Select(attrs={'class': 'form-select'}),
-    )
-
+    # Фильтр по метке (остается ModelChoiceFilter)
     labels = ModelChoiceFilter(
         queryset=Label.objects.all(),
         label=_('Метка'),
@@ -44,11 +46,25 @@ class TaskFilter(FilterSet):
         widget=forms.CheckboxInput,
     )
 
+    # --- Добавляем __init__ для генерации choices ---
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Формируем список вариантов для исполнителя: (pk, full_name)
+        # Добавляем пустой вариант в начало
+        executor_choices = [('', _('Все исполнители'))] + [
+            (user.pk, user.get_full_name()) for user in User.objects.order_by('first_name', 'last_name') # Добавим сортировку
+        ]
+        # Присваиваем сформированный список полю choices нашего фильтра executor
+        self.filters['executor'].field.choices = executor_choices
+
     def filter_self_tasks(self, queryset, name, value):
         if value:
-            return queryset.filter(author=self.request.user)
+            if hasattr(self, 'request') and self.request.user.is_authenticated:
+                 return queryset.filter(author=self.request.user)
+            return queryset.none()
         return queryset
 
     class Meta:
         model = Task
+        # Указываем поля модели ИЛИ имена наших полей фильтра
         fields = ['status', 'executor', 'labels', 'self_tasks']
--- tasks/forms.py
+++ tasks/forms.py
@@ -2,44 +2,39 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import Task
-# Импорты Status и Label не обязательны здесь, если поля обрабатываются через Meta
 
-# --- Кастомный класс поля для выбора пользователя (остается без изменений) ---
+# --- Определение UserChoiceField (здесь, один раз) ---
 class UserChoiceField(forms.ModelChoiceField):
     """
     Кастомное поле для выбора пользователя, отображающее полное имя.
     """
     def label_from_instance(self, obj):
-        # Возвращаем полное имя пользователя
         return obj.get_full_name()
 
 # --- Форма для создания и редактирования Задачи ---
 class TaskForm(forms.ModelForm):
-    # !!! УБИРАЕМ явное определение поля executor отсюда !!!
-    # executor = UserChoiceField(...) # <-- ЭТОЙ СТРОКИ БОЛЬШЕ НЕТ
+    # Используем наш кастомный класс и ЯВНО указываем label
+    executor = UserChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        label='Исполнитель',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
 
     class Meta:
         model = Task
-        # Список полей остается прежним
         fields = ['name', 'description', 'status', 'executor', 'labels']
-        # Метки полей
         labels = {
             'name': 'Имя',
             'description': 'Описание',
             'status': 'Статус',
-            'executor': 'Исполнитель',
+            # 'executor': 'Исполнитель', # Убрали, т.к. label задан выше
             'labels': 'Метки',
         }
-        # Виджеты полей
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
             'status': forms.Select(attrs={'class': 'form-select'}),
-            # !!! Виджет для executor теперь определяется здесь, а не в явном поле !!!
-            'executor': forms.Select(attrs={'class': 'form-select'}),
+            # 'executor': forms.Select(attrs={'class': 'form-select'}), # Убрали, т.к. widget задан выше
             'labels': forms.SelectMultiple(attrs={'class': 'form-select'}),
-        }
-        # --- НОВОЕ: Указываем Django использовать наш кастомный класс для поля 'executor' ---
-        field_classes = {
-            'executor': UserChoiceField, # <-- Связываем поле модели 'executor' с классом UserChoiceField
         }
